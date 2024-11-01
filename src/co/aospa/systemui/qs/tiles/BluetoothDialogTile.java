b78adfd7edd9292320a7fd918d8be00c511d4e2c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-11-01 11:08:30 +0530

    PASysUI: Require unlocking to use sensitive QS tiles
    - AOD
    - BT dialog
    - Cellular, Wifi
    - Data switch
    - Powershare
    - USB tethering
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Change-Id: Ie963e56cf7376694b47ec87edcd0994a1b018a5a

--- src/co/aospa/systemui/qs/tiles/BluetoothDialogTile.java
+++ src/co/aospa/systemui/qs/tiles/BluetoothDialogTile.java
@@ -33,6 +33,7 @@ import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
@@ -62,10 +63,12 @@ public class BluetoothDialogTile extends BluetoothTile {
             ActivityStarter activityStarter,
             QSLogger qsLogger,
             BluetoothController bluetoothController,
-            BluetoothDialogFactory bluetoothDialogFactory
+            BluetoothDialogFactory bluetoothDialogFactory,
+            KeyguardStateController keyguardStateController
     ) {
         super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
-                statusBarStateController, activityStarter, qsLogger, bluetoothController);
+                statusBarStateController, activityStarter, qsLogger, bluetoothController,
+                keyguardStateController);
         mHandler = mainHandler;
         mBluetoothDialogFactory = bluetoothDialogFactory;
     }
@@ -78,7 +81,10 @@ public class BluetoothDialogTile extends BluetoothTile {
     }
 
     @Override
-    protected void handleClick(@Nullable View view) {
+    protected void handleClick(@Nullable View view, boolean keyguardShowing) {
+        if (checkKeyguard(view, keyguardShowing)) {
+            return;
+        }
         mHandler.post(() -> mBluetoothDialogFactory.create(true, view));
     }
 }
--- src/co/aospa/systemui/qs/tiles/CellularTile.java
+++ src/co/aospa/systemui/qs/tiles/CellularTile.java
@@ -54,6 +54,7 @@ import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.SignalTileView;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.qs.tiles.SecureQSTile;
 import com.android.systemui.statusbar.connectivity.IconState;
 import com.android.systemui.statusbar.connectivity.MobileDataIndicators;
 import com.android.systemui.statusbar.connectivity.NetworkController;
@@ -64,7 +65,7 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import javax.inject.Inject;
 
 /** Quick settings tile: Cellular **/
-public class CellularTile extends QSTileImpl<SignalState> {
+public class CellularTile extends SecureQSTile<SignalState> {
 
     public static final String TILE_SPEC = "cell";
 
@@ -88,10 +89,9 @@ public class CellularTile extends QSTileImpl<SignalState> {
             QSLogger qsLogger,
             NetworkController networkController,
             KeyguardStateController keyguardStateController
-
     ) {
         super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
-                statusBarStateController, activityStarter, qsLogger);
+                statusBarStateController, activityStarter, qsLogger, keyguardStateController);
         mController = networkController;
         mKeyguard = keyguardStateController;
         mDataController = mController.getMobileDataController();
@@ -117,7 +117,10 @@ public class CellularTile extends QSTileImpl<SignalState> {
     }
 
     @Override
-    protected void handleClick(@Nullable View view) {
+    protected void handleClick(@Nullable View view, boolean keyguardShowing) {
+        if (checkKeyguard(view, keyguardShowing)) {
+            return;
+        }
         if (getState().state == Tile.STATE_UNAVAILABLE) {
             return;
         }
